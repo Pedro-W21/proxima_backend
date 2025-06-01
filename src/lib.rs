c6df8fe79323af3eebbe587a6bb4833e5f995863
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-06-01 23:15:15 +0200

    trying to make it work

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use ai_interaction::{launch_ai_endpoint_thread};
 use database::launch_database_thread;
 use initialization::initialize;
 use proxima_handler::ProximaHandler;
-use openai::Credentials;
 pub mod database;
 pub mod ai_interaction;
 pub mod proxima_handler;
