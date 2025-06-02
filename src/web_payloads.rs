34e01528dc690301aecc5342106f5bc27268ce2c
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-06-02 09:22:32 +0200

    adding constructors to the requests

--- src/web_payloads.rs
+++ src/web_payloads.rs
@@ -10,7 +10,13 @@ pub struct AIPayload {
 }
 #[derive(Clone, Serialize, Deserialize)]
 pub struct AIResponse {
-    reply:EndpointResponseVariant
+    pub reply:EndpointResponseVariant
+}
+
+impl AIPayload {
+    pub fn new(auth_key:String, request:EndpointRequestVariant) -> Self {
+        Self { auth_key, request }
+    }
 }
 
 
@@ -26,7 +32,25 @@ pub struct AuthPayload {
 }
 #[derive(Clone, Serialize,Deserialize)]
 pub struct AuthResponse {
-    session_token:String,
+    pub session_token:String,
+}
+
+
+
+impl AuthPayload {
+    pub fn new(password:String, username:String) -> Self {
+        Self {
+            device_name: whoami::devicename(),
+            device_type: match std::env::consts::OS {
+                "android" | "ios" => DeviceType::Smartphone,
+                _ => DeviceType::Laptop
+            },
+            device_os: std::env::consts::OS.into(),
+            device_model: String::from("Generic computing device (I don't know man)"),
+            password_hash: password,
+            username: username 
+        }
+    }
 }
 
 
@@ -35,6 +59,13 @@ pub struct DBPayload {
     auth_key:String,
     request:DatabaseRequestVariant
 }
+
+impl DBPayload {
+    pub fn new(auth_key:String, request:DatabaseRequestVariant) -> Self {
+        Self { auth_key, request }
+    }
+}
+
 #[derive(Clone, Serialize, Deserialize)]
 pub struct DBResponse {
     reply:DatabaseReplyVariant
