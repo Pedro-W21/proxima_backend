e37ba9cf376e94273ce546b7f16b5bdc5752ee50
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-06-25 11:05:10 +0200

    changing outputs for the insert_or_update method

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -213,63 +213,76 @@ impl ProxDatabase {
             }
         }
     }
-    pub fn insert_or_update(&mut self, item:DatabaseItem) {
+    pub fn insert_or_update(&mut self, item:DatabaseItem) -> bool { //true if updated, false if inserted
         match item {
             DatabaseItem::AccessMode(access_mode) => {
                 if self.access_modes.get_modes()[access_mode.get_id()].added_on == access_mode.added_on {
                     self.access_modes.update_mode(access_mode);
+                    true
                 }
                 else {
                     self.insert_access_mode(access_mode);
+                    false
                 }
             },
             DatabaseItem::Chat(chat) => {
                 let id = chat.get_id();
                 if self.chats.get_chats().get(&id).unwrap().start_date == chat.start_date {
                     self.chats.get_chats_mut().insert(id, chat);
+                    true
                 }
                 else {
                     self.insert_chat(chat);
+                    false
                 }
             },
             DatabaseItem::Device(device) => {
                 let id = device.get_id();
                 if self.devices.get_devices()[id].added_on == device.added_on {
                     self.devices.get_devices_mut()[id] = device;
+                    true
                 }
                 else {
                     self.insert_device(device);
+                    false
                 }
             },
             DatabaseItem::File(file) => {
                 let id = file.get_id();
                 if self.files.get_file_mut(id).added_at == file.added_at {
                     *self.files.get_file_mut(id) = file;
+                    true
                 }
                 else {
                     self.insert_file(file);
+                    false
                 }
             },
             DatabaseItem::Folder(folder) => {
                 let id = folder.get_id();
                 if self.folders.get_folder_mut(id).added_at == folder.added_at {
                     *self.folders.get_folder_mut(id) = folder;
+                    true
                 }  
                 else {
                     self.insert_folder(folder);
+                    false
                 }
             },
             DatabaseItem::Tag(tag) => {
                 let id = tag.get_id();
                 if self.tags.get_tags_mut()[id].created_at == tag.created_at {
                     self.tags.get_tags_mut()[id] = tag;
+                    true
                 }
                 else {
                     self.insert_tag(tag);
+                    false
                 }
             },
             DatabaseItem::UserData(user_data) => {
-                self.personal_info.user_data = user_data
+                self.personal_info.user_data = user_data;
+                true
             }
         }
     }
