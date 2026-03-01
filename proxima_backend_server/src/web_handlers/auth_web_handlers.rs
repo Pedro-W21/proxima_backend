66ff025fab1f482a0146cd2a5fe7bd4cbbd71222
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2026-03-01 05:25:27 +0100

    correcting an issue with authentication that could result in timeouts when HTTP timeout was less than the time to get to a normal database request internally

--- proxima_backend_server/src/web_handlers/auth_web_handlers.rs
+++ proxima_backend_server/src/web_handlers/auth_web_handlers.rs
@@ -12,23 +12,29 @@ pub async fn auth_post_handler(payload: web::Json<AuthPayload>, data: web::Data<
     // process payload and use handler
     let (request, recv) = DatabaseRequest::new(DatabaseRequestVariant::Get(DatabaseItemID::UserData), None);
     data.database.send_prio(request);
+    println!("[authentication] Sent first DB request");
     let reply = recv.recv().unwrap();
+    println!("[authentication] Received first DB response");
     match reply.variant {
         DatabaseReplyVariant::ReturnedItem(DatabaseItem::UserData(user_data)) => {
             if user_data.password_hash == payload.password_hash && user_data.pseudonym == payload.username {
                 let (request, recv) = DatabaseRequest::new(DatabaseRequestVariant::NewAuthKey, None);
                 data.database.send_prio(request);
+                println!("[authentication] Sent second DB request"); 
                 match recv.recv().unwrap().variant {
                     DatabaseReplyVariant::NewAuth(new_auth) => {
+                        println!("[authentication] Received second DB response");
                         let mut device_id = 0;
                         let (request, recv) = DatabaseRequest::new(DatabaseRequestVariant::Info(DatabaseInfoRequest::NumbersOfItems), None);
                         data.database.send_prio(request);
+                        println!("[authentication] Sent third DB request"); 
                         match recv.recv().unwrap().variant {
                             DatabaseReplyVariant::Info(DatabaseInfoReply::NumbersOfItems { devices, chats, folders, files, tags, access_modes }) => {
+                                println!("[authentication] Received third DB response");
                                 let mut found_device = false;
                                 for i in 0..devices {
                                     let (request, recv) = DatabaseRequest::new(DatabaseRequestVariant::Get(DatabaseItemID::Device(i)), None);
-                                    data.database.send_normal(request);
+                                    data.database.send_prio(request);
                                     match recv.recv().unwrap().variant {
                                         DatabaseReplyVariant::ReturnedItem(DatabaseItem::Device(device)) => {
                                             if &device.device_model == &payload.device_model && &device.device_name == &payload.device_name && &device.device_type == &payload.device_type {
@@ -42,7 +48,7 @@ pub async fn auth_post_handler(payload: web::Json<AuthPayload>, data: web::Data<
                                 }
                                 if !found_device {
                                     let (request, recv) = DatabaseRequest::new(DatabaseRequestVariant::Add(DatabaseItem::Device(Device::new(0, payload.device_name.clone(), payload.device_type.clone(), payload.device_os.clone(), payload.device_model.clone()))), None);
-                                    data.database.send_normal(request);
+                                    data.database.send_prio(request);
                                     device_id = match recv.recv().unwrap().variant {
                                         DatabaseReplyVariant::AddedItem(DatabaseItemID::Device(id)) => id,
                                         _ => panic!("Confusion on return")
@@ -51,6 +57,8 @@ pub async fn auth_post_handler(payload: web::Json<AuthPayload>, data: web::Data<
                             },
                             _ => panic!("Confusion on return")
                         }
+
+                        println!("[authentication] Successfully authenticated, sending session token"); 
                         HttpResponse::Ok().json(AuthResponse {  
                             session_token:new_auth,
                             device_id
